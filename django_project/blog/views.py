0241ea510770514e0b12d0d11da6a8425ab4c5cd
mohammadthoufeeq9 <thoufeeqa161@gmail.com>
2026-08-10 16:22:59 +0530

    templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
- 
-# Create your views here.
+posts=[
+    {
+        'author':'Thoufeeq',
+        'title':'First blog post',
+        'context':'Human emotions',
+        'date':'24,feb,2020'
+    },
+    {
+        'author':'Mohammad Thoufeeq',
+        'title':'human culture',
+        'context':'Human emotions and responsibility',
+        'date':'18-march-2020'
+    }
+] 
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context ={
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
