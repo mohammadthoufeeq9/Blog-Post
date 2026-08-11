68f8611bfd521d6dda553160a38ed5a7930d6476
mohammadthoufeeq9 <thoufeeqa161@gmail.com>
2026-08-11 22:13:02 +0530

    temp

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -21,4 +21,4 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html',{'title':'About_pg'})
