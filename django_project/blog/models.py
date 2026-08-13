3a3b2309b10d9771c32981df955995890432f056
mohammadthoufeeq9 <thoufeeqa161@gmail.com>
2026-08-13 22:19:18 +0530

    ORM-object relational mapping

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class post(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField()
+    date_posted =models.DateField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)#one to many relationship by using foreignkey
+
+    def __str__(self):
+        return self.title
