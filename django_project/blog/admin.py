0218df37384a738dbc58af3f04cbe50c2e5e9921
mohammadthoufeeq9 <thoufeeqa161@gmail.com>
2026-08-13 22:52:29 +0530

    DB,Migrations and register models

--- django_project/blog/admin.py
+++ django_project/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import post
 # Register your models here.
+
+
+admin.site.register(post)#with this reg we will able to see data in user admin...
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,22 +1,10 @@
 from django.shortcuts import render
-posts=[
-    {
-        'author':'Thoufeeq',
-        'title':'First blog post',
-        'context':'Human emotions',
-        'date':'24,feb,2020'
-    },
-    {
-        'author':'Mohammad Thoufeeq',
-        'title':'human culture',
-        'context':'Human emotions and responsibility',
-        'date':'18-march-2020'
-    }
-] 
+from .models import post
+
 
 def home(request):
     context ={
-        'posts':posts
+        'posts':post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
