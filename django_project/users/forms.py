4ddba04fecbc73478c93af5ce755956808443b34
mohammadthoufeeq9 <thoufeeqa161@gmail.com>
2026-08-18 02:47:56 +0530

    added login and logout

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User #Django's built-in authentication system
+from django.contrib.auth.forms import UserCreationForm #Django's built-in authentication system
 
 class UserRegisterForm(UserCreationForm):
     email=forms.EmailField()
