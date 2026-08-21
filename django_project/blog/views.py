07df11e2ed268040689f0368b61ced612f0279b3
mohammadthoufeeq9 <thoufeeqa161@gmail.com>
2026-08-21 22:14:00 +0530

    updated class based views

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import post
 
 
@@ -10,3 +11,9 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html',{'title':'About_pg'})
+
+class PostListView(ListView):
+    model = post
+    template_name='blog/home.html'  #<app>/<model>_<viewtype>.html
+    context_object_name='posts'
+    ordering=['-date_posted']# this will make the order of the posts from new to old.
