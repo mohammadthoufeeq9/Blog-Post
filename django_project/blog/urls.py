644e846ee5ac956636e94c7f657257dd8ec3ce49
mohammadthoufeeq9 <thoufeeqa161@gmail.com>
2026-08-22 17:20:16 +0530

    update feature by using UserPassesTestMixin

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
     PostListView,
     PostDetailView,
-    PostCreateView
+    PostCreateView,
+    PostUpdateView
 )
 from . import views
 urlpatterns = [
@@ -10,5 +11,6 @@ urlpatterns = [
     path('about/', views.about, name='blog-about'),
     path('post/int:<pk>/',PostDetailView.as_view(),name='post-detail'),
     path('post/new/',PostCreateView.as_view(),name='post-create'),#cannot name template as post_create.html because CreateView automatically uses the pattern:<app_name>/<model_name>_form.html
+    path('post/int:<pk>/update/',PostUpdateView.as_view(),name='post-update'),#Django handles the form logic but we still need a template such as:blog/post_form.html
 ]
 #passwordistoocommon
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin #Ensures that only logged-in users can access a class-based view. If the user isn't logged in, Django redirects them to the login page.
+# userPassesTestMixin-Allows access to a class-based view only if a condition define returns True.
 from django.views.generic import (
     ListView,
     DetailView,
-    CreateView
+    CreateView,
+    UpdateView,
 )
 from .models import post
 
@@ -25,10 +28,25 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin,CreateView):
     model=post
     fields=['title','content']
 
     def form_valid(self,form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model=post
+    fields=['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
